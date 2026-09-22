a5704fd1eabbef67db063494302f2c0d970ed89e
SammyEnigma <samthebest1993@gmail.com>
2026-09-22 17:25:00 +0000

    v17: use P0_ORACLE_PRODUCTION_SLOT=1 for fops stage

--- src/targets/pa3q-S938BXXS5AYG4/target.h
+++ src/targets/pa3q-S938BXXS5AYG4/target.h
@@ -216,3 +216,8 @@
 #define PIPE_DRAIN_SLABS   4
 
 #endif /* OFFSET_H */
+
+/* Use P0_ORACLE_PRODUCTION_SLOT instead of slot 0 for fops stage.
+ * Reference pattern from e1s/r12s targets. */
+#define APP_FOPS_REUSE_VERIFIED_PAGE 1
+#define P0_ORACLE_PRODUCTION_SLOT 1
